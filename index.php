0bfdb8f7a97163775459032007a2a0fa0a8c118c
Ben Culen <benjaminjcullen1@gmail.com>
2023-01-13 12:36:08 +0000

    refactored router to use array or lookup table

--- index.php
+++ index.php
@@ -2,18 +2,18 @@
 
 require "functions.php";
 
-$uri = $_SERVER['REQUEST_URI'];
+$uri = parse_url($_SERVER['REQUEST_URI'])["path"];
 
-if ($uri === '/')
+
+
+$routes = [
+    '/' => 'controllers/index.php',
+    '/about' => 'controllers/about.php',
+    '/contact' => 'controllers/contact.php',
+    '/mission' => 'controllers/mission.php'
+];
+
+if (array_key_exists($uri, $routes))
 {
-    require "controllers/index.php";
-} else if ($uri === '/about')
-{
-    require "controllers/about.php";
-} else if ($uri === '/contact')
-{
-    require "controllers/contact.php";
-} else if ($uri === '/mission')
-{
-    require "controllers/mission.php";
+    require $routes[$uri];
 }
